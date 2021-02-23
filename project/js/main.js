6f9daa21a1415a1b53802ffd187297ebd7c0bc4a
Aleksandr Voronov <aleksandr2220052@gmail.com>
2021-02-23 20:41:31 +0500

    lesson3 project1

--- project/js/main.js
+++ project/js/main.js
@@ -43,12 +43,12 @@ class ProductItem {
     }
 
     render() {
-        return `<div class="product-item" data-id = "${this.id}">
+        return `<div class="product-item">
                  <img src="${this.img}" alt="${this.title}">
                  <div class="desc">
                      <h3>${this.title}</h3>
                      <p>${this.price}</p>
-                     <button class="buy-btn">Купить</button>
+                     <button class="buy-btn" data-id = "${this.id}">Купить</button>
                  </div>
              </div>`
     }
@@ -56,32 +56,73 @@ class ProductItem {
 
 
 class Cart {
-    // some - cartItems array
+    cartProducts =[];
+    container = null;
 
-    // openPreviewCart() - открыть предварительный просмотр корзины (выпадающая корзина). Обработка события наведения на кнопку корзины ( по типу hover)
+    constructor(selector) {
+        this.container = document.querySelector(selector);
+        this._openCart();
+        this._addItem();
+    }
 
-    // openPageCart() - открыть страницу корзины. Обработка клика по кнопке "Корзина" или по кнопке "Перейти в корзину" в предварительном просмотре
+    _openCart(){
+        document.querySelector('.btn-cart').addEventListener('click', () => {
+           document.querySelector('.cart').classList.toggle('hidden');
+        });
+    }
 
     // addItem() - добавление товара в корзину. Обработка клика по кнопке купить и добаление объекта товара в массив cartItems array
 
+    _addItem(){
+       document.querySelectorAll('.buy-btn').forEach(el => { el.addEventListener('click',  () => {
+           //
+           // this.cartProducts.push(cartProduct);
+           // this.container.insertAdjacentHTML('afterbegin', cartProduct.render())
+           })
+       })
+    }
+
     //_deleteItem() -  удаление товара из корзины. Обработка клика по кнопке "Удалить", определение удаляемого товара, удаление
 
-    // _calculateCartCost() - подсчет стоимости товаров в корзине
 
+    // _calculateCartCost() - подсчет стоимости товаров в корзине
+    _calculateCartCost() {
+        return this.cartProducts.reduce((sum, itemPrice) => sum += itemPrice.price, 0 )
+    }
 }
 
 class CartItem {
+    title = '';
+    price = 0;
+    id = 0;
+    img = '';
+
+    constructor(product, img = 'https://placehold.it/50x50') {
+        ({ title: this.title, price: this.price, id: this.id } = product);
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="cart-item">
+                 <img src="${this.img}" alt="${this.title}">
+                 <div class="cart-desc">
+                     <h3>${this.title}</h3>
+                     <p>${this.price}</p>
+                     <button class="del-btn" data-id = "${this.id}">X</button>
+                 </div>
+             </div>`
+    }
     // some - cartItems array
 
-    // renderPreview() -  создание разметки для выпадающей корзины
+    // render() -  создание разметки для выпадающей корзины
 
-    // renderPage() -  создание разметки для страницы корзины
 
     // _changeCountProducts() - изменение количества товара в корзине. Обработка поля input  в разметке товара и изменение общей стоимости одного товара в зависимости от его количества
 
 }
 
 const list = new Products('.products');
+const  cart = new Cart('.cart');
 console.log(list._sumProducts())
 
 
